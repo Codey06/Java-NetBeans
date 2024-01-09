e9ffb3430d9880f8e4004c5f2b15c6dd4499cb9c
Mustaf Jirow <154862263+Codey06@users.noreply.github.com>
2024-01-09 06:49:05 -0800

    Add files via upload

---
+++ NetBeansProjects/Lesson1/src/lesson1/Lesson1.java
@@ -0,0 +1,33 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class Lesson1 {
+
+    public static void main(String[] args) {
+      
+        
+//        System.out.println("thr total is "+sum);
+         Scanner kb =new Scanner(System.in);
+//         System.out.println("Enter Number 1");
+//        int a=kb.nextInt();
+//        System.out.println("Enter Number 2");
+//        int b=kb.nextInt();
+//        int sum=a+b;
+//        System.out.println("thr total is "+sum);
+           
+//           Assignment 1
+//        System.out.println("Enter Number ");
+//        int a=kb.nextInt();
+//        int sum=a*a;
+//        System.out.println("Jibaarku waa "+sum);
+//            Assignmen 2
+            System.out.println("Gali Sanadka aad Dhalatay");
+            int a=kb.nextInt();
+            int sum=2024-a;
+            System.out.println("Da Daadu waa " +sum+ " Sano");
+         
+      
+    }
+
+}
