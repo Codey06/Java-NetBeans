4fe6bd5067aba6b608864fc0dde3fb9079c0f569
Mustaf Jirow <154862263+Codey06@users.noreply.github.com>
2024-01-09 20:24:56 -0800

    Add files via upload

---
+++ NetBeansProjects/Lesson2/src/lesson2/Lesson2.java
@@ -0,0 +1,21 @@
+package lesson2;
+public class Lesson2 {
+    public static void main(String[] args) {
+//        int n=29;
+//        int r=n%6;
+         int n=17;
+        int r=n%7;
+        System.out.println(++r);
+/**
+ *  class ClassName{
+ * ...... instance variable , Methods, and constructor
+ * 
+ * }
+ * 
+ * public void methodName(){
+ * }
+ */
+        
+    }
+    
+}
